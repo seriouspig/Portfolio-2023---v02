69e4814e4c55d48006b0229c7b3208eefe9dc6c1
Piotr Gryko <pigryko@yahoo.com>
2023-02-23 19:53:12 +0000

    Resume added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./components/pages/home/Home";
 import About from "./components/pages/about/About";
 import Projects from "./components/pages/projects/Projects";
+import Resume from "./components/pages/resume/Resume";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/portfolio" element={<Projects />} />
-        {/* <Route exact path="/resume" element={<Resume />} /> */}
+        <Route exact path="/resume" element={<Resume />} />
       </Routes>
       <Footer />
     </Router>
