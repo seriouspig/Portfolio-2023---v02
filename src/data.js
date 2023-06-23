245df8c58b390fbfd5af405011cec9bdc6c2839c
Piotr Gryko <pigryko@yahoo.com>
2023-06-23 15:44:17 +0100

    Updated data

--- src/data.js
+++ src/data.js
@@ -2,8 +2,10 @@ import React from "react";
 import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
 import millionaires_img from "./assets/images/projects/wwtbam.png";
 import grooooaar_img from "./assets/images/projects/grooooaar.gif";
+import they_keep_coming_img from "./assets/images/projects/theykeepcoming.gif";
 import idefender_img from "./assets/images/projects/idefender.gif";
 import ward_img from "./assets/images/projects/theward.png";
+import dungeon_master from "./assets/images/projects/dungeonmaster.png";
 export const pageLinks = [
   {
     id: 1,
@@ -48,7 +50,7 @@ export const socialLinks = [
 export const projects = [
   {
     id: 1,
-    url: "https://www.onet.pl",
+    url: "https://seriouspig.itch.io/id-efencder",
     image: idefender_img,
     title: "Id-efender",
     info: "A solo project developed as part of my Game Development Course at Edinburgh College. A spin on the 1981 Atari’s classic “Defender”.",
@@ -56,7 +58,7 @@ export const projects = [
   },
   {
     id: 2,
-    url: "https://www.onet.pl",
+    url: "https://millionaires-c4191.web.app",
     image: millionaires_img,
     title: "Millionaires",
     info: "Group project developed during the CodeClan boot camp course. Based on the TV show – 'Who wants to be a millionaire'.",
@@ -64,7 +66,7 @@ export const projects = [
   },
   {
     id: 3,
-    url: "https://www.onet.pl",
+    url: "https://the-ward-c50f7.web.app/",
     image: ward_img,
     title: "The Ward",
     info: "Another entry for the itch.io Nokia 3310 game jam. Interesting enemy AI. A full-res version for mobile devces is in the works",
@@ -72,7 +74,7 @@ export const projects = [
   },
   {
     id: 4,
-    url: "https://www.onet.pl",
+    url: "https://madbatgames.itch.io/grooooarr",
     image: grooooaar_img,
     title: "GROOOOARR",
     info: "Another entry for the itch.io Nokia 3310 game jam. Interesting enemy AI. A full-res version for mobile devces is in the works",
@@ -80,27 +82,27 @@ export const projects = [
   },
   {
     id: 5,
-    url: "https://www.onet.pl",
-    image: idefender_img,
-    title: "Id-efender",
-    info: "A solo project developed as part of my Game Development Course at Edinburgh College. A spin on the 1981 Atari’s classic “Defender”.",
+    url: "https://seriouspig.itch.io/they-keep-coming",
+    image: they_keep_coming_img,
+    title: "THEY KEEP COMING",
+    info: "A simple game developed for the itch.io Nokia 3310 game jam. The game achieved 10th place out of almost 400 entries. ",
     category: "Games",
   },
   {
     id: 6,
-    url: "https://www.onet.pl",
-    image: millionaires_img,
-    title: "Millionaires",
-    info: "Group project developed during the CodeClan boot camp course. Based on the TV show – 'Who wants to be a millionaire'.",
-    category: "Software",
+    url: "https://github.com/seriouspig/Dungeon-Master",
+    image: dungeon_master,
+    title: "DUNGEON MASTER 2D",
+    info: "A simple remake of the old DOS classic - Dungeon Master. All done in Vanilla Javascript. A full 3d version using Three.js is in the works",
+    category: "Games",
   },
   {
     id: 7,
     url: "https://www.onet.pl",
     image: ward_img,
-    title: "The Ward",
-    info: "Another entry for the itch.io Nokia 3310 game jam. Interesting enemy AI. A full-res version for mobile devces is in the works",
-    category: "Arch",
+    title: "DUNGEON MASTER 3D",
+    info: "A horror spin on the dungeon crawler genre. Full 3D using Three.js framework",
+    category: "Games",
   },
   {
     id: 8,
